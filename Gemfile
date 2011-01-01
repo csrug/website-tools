0a626fdcdd89c57769bf626b5c0ab7634f9cdd39
Karel Minarik <karmi@karmi.cz>
2011-01-02 00:39:56 +0100

    Added endpoint for building Rails guides from HTTP hook

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'http://rubygems.org'
 
 gem 'rake'
 gem 'sinatra'
+
+group :guides do
+  gem 'RedCloth'
+  gem 'i18n'
+  gem 'erubis'
+end
--- application.rb
+++ application.rb
@@ -12,6 +12,17 @@ module RubyonrailsCz
       username == Config::USERNAME && password == Config::PASSWORD
     end
 
+    post '/build' do
+      cmd = []
+      cmd << "cd #{Config::GUIDES}"
+      cmd << "rm -rf output/cz"
+      cmd << "git fetch origin"
+      cmd << "git reset origin/czech --hard"
+      cmd << "rake generate_guides ONLY=getting_started LANG=cz"
+      puts "Running command: " + cmd.join(' && ')
+      system cmd.join(' && ')
+    end
+
   end
 end
 
